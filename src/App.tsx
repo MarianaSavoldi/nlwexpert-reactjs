f32a20ea502bf8d566084e5445bf500e554d9dc3
MarianaSavoldi <marianasavodi123@gmail.com>
2024-02-10 02:05:27 -0300

    feat: add a function to delete notes

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,13 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray));
   };
 
+  const onDeleteNote = (id: string) => {
+    const notesArray = notes.filter(note => note.id !== id);
+
+    setNotes(notesArray);
+    localStorage.setItem('notes', JSON.stringify(notesArray));
+  };
+
   const filteredNotes = search !== ''
     ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
     : notes;
@@ -57,7 +64,7 @@ export function App() {
       <div className='h-px bg-slate-700' />
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6'>
         <NewNoteCard onCreateNewNote={ onCreateNewNote } />
-        { filteredNotes.map(note => <NoteCard key={ note.id } note={ note } />) }
+        { filteredNotes.map(note => <NoteCard key={ note.id } note={ note } onDeleteNote={ onDeleteNote } />) }
       </div>
     </div>
   );
